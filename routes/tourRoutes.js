b406670b44dfaaf033f3a506f21793e95aac4f7f
Rishabh@iv5394 <boletorishabh1999@gmail.com>
2023-11-09 12:17:21 +0530

    fix the code

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,26 +11,9 @@ const router = express.Router();
 // GET /tour/234fad4/reviews
 
 router.use('/:tourId/reviews', reviewRouter);
-
-router
-  .route('/top-5-cheap')
-  .get(tourController.aliasTopTours, tourController.getAllTours);
-
-router.route('/tour-stats').get(tourController.getTourStats);
-router
-  .route('/monthly-plan/:year')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide', 'guide'),
-    tourController.getMonthlyPlan
-  );
-
-router
-  .route('/tours-within/:distance/center/:latlng/unit/:unit')
-  .get(tourController.getToursWithin); 
-
-router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
-
+ 
+ 
+ 
 router
   .route('/')
   .get(tourController.getAllTours)
